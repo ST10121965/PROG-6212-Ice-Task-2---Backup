4605958502c69983d7c2e6578296fcdb741229de
Luke Davis <ST10121965@vcconnect.edu.za>
2024-09-16 16:14:44 +0200

    COmmit 1 of 1 of PROG6212 Ice Task
    Backup in case zip file doesnt work

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IceTask2_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // I should clear the richtextboxes first,
+            //before continueing with the button click event
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+
+            unsafe
+            {
+                // Create 3 integer variables, which are needed for 
+                //pointers of type int to point to
+                int a = 10, b = 20, c = 30;
+
+                // Create pointers to these variables
+                int* pointar1 = &a;
+                int* pointar2 = &b;
+                int* pointar3 = &c;
+
+                // After a button is pressed,
+                //I should display text content and addresses of the pointers
+                richTextBox1.AppendText("3 Pointers:\n");
+                richTextBox1.AppendText($"Value of the int variable named 'a': {a}, Address: {(IntPtr)pointar1}\n");
+                richTextBox1.AppendText($"Value of teh int variable named 'b': {b}, Address: {(IntPtr)pointar2}\n");
+                richTextBox1.AppendText($"Value of the int variable named 'c': {c}, Address: {(IntPtr)pointar3}\n");
+
+                // I need to create an array of 12 integers, which
+                //which this would be used by the 2nd richtaxtbox
+                int[] arr = new int[12];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = (i + 1); // * 10;  // Populate the array with 12 numbers
+                }
+
+                fixed (int* ptrArr = arr)
+                {
+                    // mext i will isplay the array elements and their addresses in RichTextBox2
+                    richTextBox2.AppendText("Array of 12 Integers:\n");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        int* currentPtr = ptrArr + i;  // Pointer on left is given a job to
+                                                       // points  to each element of the array
+                        richTextBox2.AppendText($"Value at index {i}: {arr[i]}, Address: {(IntPtr)currentPtr}\n");
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IceTask2_WindowsFormsApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IceTask2_WindowsFormsApp")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b1e46c4a-8b39-4e47-ab2b-3cd3f5014b5e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
